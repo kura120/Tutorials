9bba539d96f0b88db2a7a12fad34fbec0f843f7c
kura <89491373+kuraise@users.noreply.github.com>
2022-10-04 10:54:04 -0400

    Update TweenLib.lua

--- scripts/TweenLib.lua
+++ scripts/TweenLib.lua
@@ -1,11 +1,45 @@
 local tbl = {}
 local tween = game:GetService("TweenService")
 local tweeninfo = TweenInfo.new
+local errorSystem = {}
 
 -- will add more soon.
 
-function tbl:tweenLib(o: Instance, p, d: TweenInfo, ...)
+local function isCallable(value)
+	if type(value) == "function" then
+		return true
+	end
+
+	if type(value) == "table" then
+		local metatable = getmetatable(value)
+		if metatable and type(rawget(metatable, "__call")) == "function" then
+			return true
+		end
+	end
+
+	return false
+end
+
+
+function tbl:new(o: Instance, p, d: TweenInfo, ...)
 	return tween:Create(o, tweeninfo(d, ...), p)
+end
+do 
+    -- Error system cuz funni
+    errorSystem.new = function(callType, reason, callback)
+        assert(type(callType) == "string", "Bad argument #1 to errorSystem.new: must be a string") 
+        assert(type(reason) == "string", "Bad argument #2 to errorSystem.new: must be a string  ")
+        assert(isCallable(callback), "Bad argument #3 to errorSystem.new: must be a function (callback)")
+
+        table.insert(errorSystem.err, callType)
+        errorSystem.err[callType] = reason
+
+        callback()
+    end
+
+    errorSystem.new("Woah", "Testing", function()
+        
+    end)
 end
 
 return tbl
