4ad27c115fc9d2fdae654c6cb9e73698b13fdc90
kura <89491373+kuraise@users.noreply.github.com>
2022-10-12 08:52:53 -0400

    Tween Lib v0.0.3 Testing ( Not even tested LMAO, just coded)

--- scripts/TweenLib.lua
+++ scripts/TweenLib.lua
@@ -24,22 +24,35 @@ end
 function tbl:new(o: Instance, p, d: TweenInfo, ...)
 	return tween:Create(o, tweeninfo(d, ...), p)
 end
-do 
+
+do
     -- Error system cuz funni
-    errorSystem.new = function(callType, reason, callback)
+    errorSystem.new = function(callType, callback)
         assert(type(callType) == "string", "Bad argument #1 to errorSystem.new: must be a string") 
         assert(type(reason) == "string", "Bad argument #2 to errorSystem.new: must be a string  ")
         assert(isCallable(callback), "Bad argument #3 to errorSystem.new: must be a function (callback)")
-
         table.insert(errorSystem.err, callType)
-        errorSystem.err[callType] = reason
+        errorSystem.err[callType] = callback
 
-        callback()
+    end
+    --// Inserting functions
+    errorSystem.new("Woah", function(t, y)
+        error("Testing Error System!")
+    end)
+
+    --// Actual error system
+
+    errorSystem.find = function(errName)
+        if table.find(errorSystem, errName) then
+            return true
+        end
     end
 
-    errorSystem.new("Woah", "Testing", function()
-        
-    end)
+    local function executeCallback(callback) errorSystem.err[callback]() end -- if this doesn't work i will literally oblibirate my soul.
+
+    errorSystem.throw = function(errName)
+        if errorSystem.find(errName) then executeCallback(errName) end -- should work or ima kms
+    end
 end
 
 return tbl
